444e705547b2f4bbd4e998a7930831a1e6e617be
kevin taylor <33230433+Kevin-Lee-Taylor@users.noreply.github.com>
2023-09-25 10:03:55 -0400

    finished adding psuedo code to main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // assign 3 variable for a combination lock and display text showing the resulting combination. 
 
+// algorithm for the first number 
 let firstNum = 5 + 5;
 
+// algorithm for the second number
 let secondNum = 10 * 4;
 
+//algorithm for the third number
 let thirdNum = 50-11;
 
-const string = You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstNum} - ${secondNum} - ${thirdNum}`;
+// display the message string for the for vault combination
+const string = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstNum} - ${secondNum} - ${thirdNum}`;
 
+// finally the alert/pop up for the message and combination
 alert(string);
