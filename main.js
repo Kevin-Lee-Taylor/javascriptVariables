e2ce0683ba7f26290e39cca9eca03262b89006da
kevin taylor <33230433+Kevin-Lee-Taylor@users.noreply.github.com>
2023-09-25 09:31:29 -0400

    pseudo code and assigned variables and string added to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// assign 3 variable for a combination lock and display text showing the resulting combination. 
+
+let firstNum = 5 + 5;
+
+let secondNum = 10 * 4;
+
+let thirdNum = 50-11;
+
+const string = You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstNum} - ${secondNum} - ${thirdNum}`;
+
+alert(string);
